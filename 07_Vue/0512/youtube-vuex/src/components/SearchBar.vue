5a349822003e5a24be29054c84fa20769fdaeb7c
qoqls1212 <qoqls1212@gmail.com>
2022-05-12 14:13:25 +0900

    previous project

---
+++ 07_Vue/0512/youtube-vuex/src/components/SearchBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search-bar" :style="{ margin: isVideos ? '0' : '40vh 0' }">
+    <input type="text" @keyup.enter="onEnter" placeholder="Search" autofocus />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SearchBar',
+    props: {
+      isVideos: Boolean,
+    },
+    methods: {
+      onEnter(event) {
+        this.$emit('keyword-input', event.target.value)
+      },
+    },
+  }
+</script>
+
+<style>
+  .search-bar > input {
+    width: 100%;
+    padding: 0.5rem;
+    font-size: 2rem;
+  }
+  .search-bar {
+    transition: margin 0.5s;
+  }
+</style>
---
+++ 07_Vue/0512/youtube-vuex/src/components/VideoDetail.vue
@@ -0,0 +1,64 @@
+<template>
+  <div v-if="isVideo" class="video-detail">
+    <div class="video-container">
+      <iframe :src="videoSrc" frameborder="0"></iframe>
+    </div>
+
+    <div class="video-info">
+      <h2>{{ video.snippet.title | strUnescape }}</h2>
+      <p>{{ video.snippet.description | strUnescape }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import _ from 'lodash'
+
+  export default {
+    name: 'VideoDetail',
+    props: {
+      video: Object,
+    },
+    computed: {
+      videoSrc() {
+        const videoId = this.video.id?.videoId
+        return `https://www.youtube.com/embed/${videoId}`
+      },
+      isVideo() {
+        return !_.isEmpty(this.video)
+      },
+    },
+    filters: {
+      strUnescape(rawText) {
+        return _.unescape(rawText)
+      },
+    },
+  }
+</script>
+
+<style>
+  .video-detail {
+    width: 70%;
+    padding-right: 1rem;
+  }
+
+  .video-container {
+    position: relative;
+    padding-top: 56.28%;
+  }
+
+  .video-container > iframe {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  .video-info {
+    margin-top: 20px;
+    padding: 20px;
+    border: solid 1px lightgray;
+    border-radius: 10px;
+  }
+</style>
