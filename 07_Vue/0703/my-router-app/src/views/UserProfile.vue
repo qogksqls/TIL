225c84566d51fc7b7f3fda74b6a666bada970d41
qoqls1212 <qoqls1212@gmail.com>
2022-07-03 19:00:21 +0900

    vue router 기초 공부

---
+++ 07_Vue/0703/my-router-app/src/views/UserProfile.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>User Profile</h1>
+    <h1>내 아이디: {{user.userId}}</h1>
+    <h1>내 이름: {{user.username}}</h1>
+    <h1>내 전공: {{user.major}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserProfile",
+  data: function () {
+    return {
+      user: this.$route.params,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
